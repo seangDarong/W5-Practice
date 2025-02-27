f976cf9e08f2b6671a4b7e71696eb83cd1ea49e4
rong <darong6126@gmail.com>
2025-02-27 11:14:24 +0700

    done

---
+++ exercise2/src/App.jsx
@@ -0,0 +1,26 @@
+import {useState} from 'react'
+
+import vitelogo from '/vite.svg'
+import Header from '../components/Header.jsx'
+import Main from '../components/Main.jsx'
+import Footer from '../components/Footer.jsx'
+
+// function App(){
+// return (
+//     <>
+//     <p>hello World</p>
+//     </>
+// )
+// }
+
+function App(){
+    return(
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
+    )
+}
+
+export default App
